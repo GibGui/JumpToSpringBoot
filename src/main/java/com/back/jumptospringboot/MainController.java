f3832483906b7ef4cc3257da066dde82976eba4b
GibGui <hsy17026@gmail.com>
2026-08-06 16:32:25 +0900

    메인 컨트롤러 이름 의미있게 수정

--- src/main/java/com/back/jumptospringboot/MainController.java
+++ src/main/java/com/back/jumptospringboot/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
-public class PersonController {
+public class MainController {
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
