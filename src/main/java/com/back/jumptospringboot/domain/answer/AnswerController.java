ea600548a8fbdd18eb2421247ce4534c5f30cd29
GibGui <hsy17026@gmail.com>
2026-08-10 23:03:00 +0900

    2-16 질문 등록 기능 추가

--- src/main/java/com/back/jumptospringboot/domain/answer/AnswerController.java
+++ src/main/java/com/back/jumptospringboot/domain/answer/AnswerController.java
@@ -3,13 +3,14 @@ package com.back.jumptospringboot.domain.answer;
 
 import com.back.jumptospringboot.domain.question.Question;
 import com.back.jumptospringboot.domain.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,18 +19,22 @@ public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
+
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id,
-                               @RequestParam(value = "content") String content){
+                               @Valid AnswerForm answerForm, BindingResult bindingResult ) {
 
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
+        if(bindingResult.hasErrors()){
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
 
 
     }
-
-
 
 
 }
--- src/main/java/com/back/jumptospringboot/domain/question/QuestionService.java
+++ src/main/java/com/back/jumptospringboot/domain/question/QuestionService.java
@@ -5,6 +5,7 @@ import com.back.jumptospringboot.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +27,13 @@ public class QuestionService {
             throw new DataNotFoundException("question not found");
         }
 
+    }
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 
 
