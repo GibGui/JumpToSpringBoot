4e1e4e747df4ab1ac4c9dc49aecd4a20a3bf98ea
GibGui <hsy17026@gmail.com>
2026-08-19 00:21:48 +0900

    3-11 답변 앵커기능 추가

--- src/main/java/com/back/jumptospringboot/domain/answer/controller/AnswerController.java
+++ src/main/java/com/back/jumptospringboot/domain/answer/controller/AnswerController.java
@@ -45,9 +45,10 @@ public class AnswerController {
             return"question_detail";
         }
 
-        this.answerService.create(question, answerForm.getContent(), siteUser);
-
-        return String.format("redirect:/question/detail/%s",id);
+        Answer answer = this.answerService.create(question,
+                answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
 
     }
 
@@ -84,8 +85,8 @@ public class AnswerController {
         }
         this.answerService.modify(answer, answerForm.getContent());
 
-        return String.format("redirect:/question/detail/%s",
-                answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -116,8 +117,8 @@ public class AnswerController {
 
         this.answerService.vote(answer, siteUser);
 
-        return String.format("redirect:/question/detail/%s",
-                answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 
 }
--- src/main/java/com/back/jumptospringboot/domain/answer/service/AnswerService.java
+++ src/main/java/com/back/jumptospringboot/domain/answer/service/AnswerService.java
@@ -18,7 +18,7 @@ public class AnswerService {
     private final AnswerRepository answerRepository;
 
 
-    public void create(Question question, String content, SiteUser author){
+    public Answer create(Question question, String content, SiteUser author){
 
         Answer answer = new Answer();
         answer.setQuestion(question);
@@ -26,7 +26,7 @@ public class AnswerService {
         answer.setAuthor(author);
         answer.setCreateDate(LocalDateTime.now());
         this.answerRepository.save(answer);
-
+        return answer;
     }
 
     public Answer getAnswer(Integer id){
